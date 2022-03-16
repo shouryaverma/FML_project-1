a34b027a9e47c167cac4564b25d56807cb217890
Cem Daloglu <chemdaloglu@gmail.com>
2022-03-16 19:23:44 +0300

    Make rule based agents compatible with board game size changes

---
+++ agent_code/my_agent_1/callbacks.py
@@ -0,0 +1,294 @@
+import os
+import pickle
+import random
+
+import numpy as np
+
+ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
+STATE = 17
+EXPLORATION_RATE = 0.1
+EXPLORE_COUNT = 0
+EXPLOIT_COUNT = 0
+
+
+def setup(self):
+    """
+    Setup your code. This is called once when loading each agent.
+    Make sure that you prepare everything such that act(...) can be called.
+
+    When in training mode, the separate `setup_training` in train.py is called
+    after this method. This separation allows you to share your trained agent
+    with other students, without revealing your training code.
+
+    In this example, our model is a set of probabilities over actions
+    that are is independent of the game state.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+    self.exploration_rate = EXPLORATION_RATE
+    if self.train or not os.path.isfile("my-saved-model.pt"):
+        self.logger.info("Setting up model from scratch.")
+        weights = np.random.rand(len(ACTIONS))
+        self.model = weights / weights.sum()
+    else:
+        self.logger.info("Loading model from saved state.")
+        with open("my-saved-model.pt", "rb") as file:
+            self.model = pickle.load(file)
+
+    self.logger.info("Loading Q_sa_inc_state_2 function from saved state.")
+    with open('Q_sa_inc_state_2.npy', 'rb') as f:
+        self.q_sa = np.load(f, allow_pickle=True)
+    self.q_sa = self.q_sa.tolist()
+
+
+def act(self, game_state: dict) -> str:
+    """
+    Your agent should parse the input, think, and take a decision.
+    When not in training mode, the maximum execution time for this method is 0.5s.
+
+    :param self: The same object that is passed to all of your callbacks.
+    :param game_state: The dictionary that describes everything on the board.
+    :return: The action to take as a string.
+    """
+    global EXPLORE_COUNT
+    global EXPLOIT_COUNT
+    q = self.q_sa
+    cond = np.max(list(q[features_to_index(state_to_features(game_state))].values()))
+    if cond < self.exploration_rate:
+        # explore
+        cordin = np.array(game_state['self'][3])
+        x = cordin[1]
+        y = cordin[0]
+        arena = np.array(game_state['field']).T
+        bomb_map = np.ones(arena.shape) * 5
+        bomb_xys = [xy for (xy, t) in game_state['bombs']]
+        if bomb_xys:
+            bomb_xys = [(bomb_xys[0][1], bomb_xys[0][0])]
+
+        for (yb, xb), t in game_state['bombs']:
+            for (i, j) in [(xb + h, yb) for h in range(-3, 4)] + [(xb, yb + h) for h in range(-3, 4)]:
+                if (0 < i < bomb_map.shape[0]) and (0 < j < bomb_map.shape[1]):
+                    bomb_map[i, j] = min(bomb_map[i, j], t)
+
+        self.logger.debug("Exploring random action")
+        self.logger.debug(f"Random Q: {list(q[features_to_index(state_to_features(game_state))].values())}")
+        self.logger.debug(f"Explore index: {features_to_index(state_to_features(game_state))}")
+
+        # determine valid actions
+        directions = [(x, y), (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+        valid_tiles, valid_actions = [], []
+        for d in directions:
+            d_trans = (d[1], d[0])
+            if ((arena[d] == 0) and
+                    (game_state['explosion_map'][d_trans] < 1) and
+                    (bomb_map[d] > 0) and
+                    (not d in bomb_xys)):
+                valid_tiles.append(d)
+        if (x - 1, y) in valid_tiles: valid_actions.append('UP')
+        if (x + 1, y) in valid_tiles: valid_actions.append('DOWN')
+        if (x, y - 1) in valid_tiles: valid_actions.append('LEFT')
+        if (x, y + 1) in valid_tiles: valid_actions.append('RIGHT')
+        if (x, y) in valid_tiles: valid_actions.append('WAIT')
+        if game_state['self'][2]: valid_actions.append('BOMB')
+
+        self.logger.debug(f'Valid actions: {valid_actions}')
+        if len(valid_actions) == 0:
+            action = 'WAIT'
+            EXPLORE_COUNT += 1
+        else:
+            max_act = ACTIONS[np.argmax(list(q[features_to_index(state_to_features(game_state))].values()))]
+            if max_act in valid_actions:
+                EXPLOIT_COUNT += 1
+                action = max_act
+            else:
+                EXPLORE_COUNT += 1
+                action = np.random.choice(valid_actions)
+    else:
+        # exploit
+        EXPLOIT_COUNT += 1
+        self.logger.debug("Exploiting (predict actions)")
+        self.logger.debug(f"Q: {list(q[features_to_index(state_to_features(game_state))].values())}")
+        self.logger.debug(f"Index: {features_to_index(state_to_features(game_state))}")
+        action = ACTIONS[np.argmax(list(q[features_to_index(state_to_features(game_state))].values()))]
+
+    self.logger.debug(f"Explore count: {EXPLORE_COUNT}, Exploit count: {EXPLOIT_COUNT}")
+    self.logger.debug(f"Took action {action}")
+    return action
+
+
+def state_to_features(game_state: dict) -> np.array:
+    """
+    *This is not a required function, but an idea to structure your code.*
+
+    Converts the game state to the input of your model, i.e.
+    a feature vector.
+
+    You can find out about the state of the game environment via game_state,
+    which is a dictionary. Consult 'get_state_for_agent' in environment.py to see
+    what it contains.
+
+    :param game_state:  A dictionary describing the current game board.
+    :return: np.array
+    """
+    # This is the dict before the game begins and after it ends
+    if game_state is None:
+        return None
+
+    # For example, you could construct several channels of equal shape, ...
+    # channels = []
+    # channels.append(...)
+    # concatenate them as a feature tensor (they must have the same shape), ...
+    # stacked_channels = np.stack(channels)
+
+    self_cor_channel = np.array(game_state["self"][3])
+    down_channel = [self_cor_channel[1] + 1, self_cor_channel[0]]
+    up_channel = [self_cor_channel[1] - 1, self_cor_channel[0]]
+    left_channel = [self_cor_channel[1], self_cor_channel[0] - 1]
+    right_channel = [self_cor_channel[1], self_cor_channel[0] + 1]
+
+    left = cor_states(game_state, left_channel, 'left')
+    right = cor_states(game_state, right_channel, 'right')
+    up = cor_states(game_state, up_channel, 'up')
+    down = cor_states(game_state, down_channel, 'down')
+    # self_ch = cor_states(game_state, self_cor_channel) # no need to check current position
+
+    stacked_channels = np.array([left] + [right] + [up] + [down] + [game_state["self"][2]])
+    stacked_channels = np.concatenate(stacked_channels, axis=None)
+    # and return them as a vector
+    return stacked_channels
+
+
+def cor_states(game_state, coordinates, direction_str):
+    """
+        Checks the given coordinates if there is a coin, if there is a wall or crate or free, and if there is a danger.
+        Danger also contains walls as well.
+
+        :param game_state:  A dictionary describing the current game board.
+        :param coordinates:  The given coordinates.
+        :return: np.array of size 3 -> ([0 or 1], [0 or 1], [0 or 1])
+        """
+    field_channel = np.array(game_state["field"]).T
+    coins = np.array(game_state["coins"]).T
+    bombs = np.array(game_state["bombs"]).T
+    explosion = np.array(game_state["explosion_map"])
+    state_bits = np.zeros(4)
+    if field_channel[coordinates[0], coordinates[1]] == 1:
+        state_bits[1] = 1
+    elif field_channel[coordinates[0], coordinates[1]] == 0:
+        state_bits[1] = 0
+    else:
+        state_bits[2] = 1
+    if coins.size != 0:
+        for ind in range(len(coins[0])):
+            if coins[0, ind] == coordinates[1] and coins[1, ind] == coordinates[0]:
+                state_bits[0] = 1
+    if bombs.size != 0:
+        for idx in range(len(bombs[0])):
+            check = list(bombs[idx][0])
+            if bombs[1] <= 1:
+                if (check[0] == coordinates[1] and np.abs(check[1] - coordinates[0]) < 3) or (
+                        check[1] == coordinates[0] and np.abs(
+                    check[0] - coordinates[1]) < 3):
+                    state_bits[2] = 1
+            elif bombs[1] == 2:
+                if (check[0] == coordinates[1] and np.abs(check[1] - coordinates[0]) < 2) or (
+                        check[1] == coordinates[0] and np.abs(
+                    check[0] - coordinates[1]) < 2):
+                    state_bits[2] = 1
+            if check[0] == coordinates[1] and check[1] == coordinates[0]:
+                state_bits[2] = 1
+    if explosion[coordinates[1], coordinates[0]] != 0:
+        state_bits[2] = 1
+
+    if direction_str == 'left' and (coordinates[1] > 2) and (
+            (field_channel[coordinates[0] - 1, coordinates[1]] != 0 and field_channel[
+                coordinates[0], coordinates[1] - 1] != 0 and field_channel[
+                 coordinates[0] + 1, coordinates[1]] != 0) or (
+                    field_channel[coordinates[0] - 1, coordinates[1] - 1] != 0 and
+                    field_channel[coordinates[0], coordinates[1] - 2] != 0 and field_channel[
+                        coordinates[0] + 1, coordinates[1] - 1] != 0) or (
+                    field_channel[coordinates[0] - 1, coordinates[1] - 2] != 0 and
+                    field_channel[coordinates[0], coordinates[1] - 3] != 0 and field_channel[
+                        coordinates[0] + 1, coordinates[1] - 2] != 0)):
+        state_bits[3] = 1
+    elif direction_str == 'left' and (coordinates[1] == 1 or coordinates[1] == 2) and (field_channel[coordinates[0] - 1, coordinates[1]] != 0 and field_channel[
+                coordinates[0], coordinates[1] - 1] != 0 and field_channel[
+                 coordinates[0] + 1, coordinates[1]] != 0):
+        state_bits[3] = 1
+    elif direction_str == 'right' and (coordinates[1] < 14) and ((field_channel[coordinates[0] - 1, coordinates[1]] != 0 and field_channel[
+                coordinates[0], coordinates[1] + 1] != 0 and field_channel[
+                 coordinates[0] + 1, coordinates[1]] != 0) or (
+                    field_channel[coordinates[0] - 1, coordinates[1] + 1] != 0 and
+                    field_channel[coordinates[0], coordinates[1] + 2] != 0 and field_channel[
+                        coordinates[0] + 1, coordinates[1] + 1] != 0) or (
+                    field_channel[coordinates[0] - 1, coordinates[1] + 2] != 0 and
+                    field_channel[coordinates[0], coordinates[1] + 3] != 0 and field_channel[
+                        coordinates[0] + 1, coordinates[1] + 2] != 0)):
+        state_bits[3] = 1
+    elif direction_str == 'right' and (coordinates[1] == 15 or coordinates[1] == 14) and (field_channel[coordinates[0] - 1, coordinates[1]] != 0 and field_channel[
+                coordinates[0], coordinates[1] + 1] != 0 and field_channel[
+                 coordinates[0] + 1, coordinates[1]] != 0):
+        state_bits[3] = 1
+    elif direction_str == 'up' and (coordinates[0] > 2) and (
+            (field_channel[coordinates[0] - 1, coordinates[1]] != 0 and field_channel[
+                coordinates[0], coordinates[1] + 1] != 0 and field_channel[
+                 coordinates[0], coordinates[1] - 1] != 0) or (
+                    field_channel[coordinates[0] - 1, coordinates[1] + 1] != 0 and
+                    field_channel[coordinates[0] - 2, coordinates[1]] != 0 and field_channel[
+                        coordinates[0] - 1, coordinates[1] + 1] != 0) or (
+                    field_channel[coordinates[0] - 2, coordinates[1] + 1] != 0 and
+                    field_channel[coordinates[0] - 3, coordinates[1]] != 0 and field_channel[
+                        coordinates[0] - 2, coordinates[1] + 1] != 0)):
+        state_bits[3] = 1
+    elif direction_str == 'up' and (coordinates[0] == 1 or coordinates[0] == 2) and (
+            field_channel[coordinates[0] - 1, coordinates[1]] != 0 and field_channel[
+        coordinates[0], coordinates[1] + 1] != 0 and field_channel[coordinates[0], coordinates[1] - 1] != 0):
+        state_bits[3] = 1
+    elif direction_str == 'down' and (coordinates[0] < 14) and (
+            (field_channel[coordinates[0] + 1, coordinates[1]] != 0 and field_channel[
+                coordinates[0], coordinates[1] + 1] != 0 and field_channel[
+                 coordinates[0], coordinates[1] - 1] != 0) or (
+                    field_channel[coordinates[0] + 1, coordinates[1] + 1] != 0 and
+                    field_channel[coordinates[0] + 2, coordinates[1]] != 0 and field_channel[
+                        coordinates[0] + 1, coordinates[1] + 1] != 0) or (
+                    field_channel[coordinates[0] + 2, coordinates[1] + 1] != 0 and
+                    field_channel[coordinates[0] + 3, coordinates[1]] != 0 and field_channel[
+                        coordinates[0] + 2, coordinates[1] + 1] != 0)):
+        state_bits[3] = 1
+    elif direction_str == 'down' and (coordinates[0] == 15 or coordinates[0] == 14) and (
+            field_channel[coordinates[0] + 1, coordinates[1]] != 0 and field_channel[
+        coordinates[0], coordinates[1] + 1] != 0 and field_channel[coordinates[0], coordinates[1] - 1] != 0):
+        state_bits[3] = 1
+
+    return state_bits
+
+
+def features_to_index(features):
+    state_no = STATE
+    index = 0
+    for n in range(state_no):
+        index += 2 ** (state_no - n - 1) * features[n]
+    return index
+
+
+def get_explosion_xys(start, map, bomb_power=3):
+    """
+    returns all tiles hit by an explosion starting at start given a 2d map of the game
+       where walls are indicated by -1
+    """
+    x, y = start
+    expl = [(x, y)]
+    for i in range(1, bomb_power + 1):
+        if map[x + i, y] == -1: break
+        expl.append((x + i, y))
+    for i in range(1, bomb_power + 1):
+        if map[x - i, y] == -1: break
+        expl.append((x - i, y))
+    for i in range(1, bomb_power + 1):
+        if map[x, y + i] == -1: break
+        expl.append((x, y + i))
+    for i in range(1, bomb_power + 1):
+        if map[x, y - i] == -1: break
+        expl.append((x, y - i))
+
+    return np.array(expl)
---
+++ agent_code/my_agent_1/train.py
@@ -0,0 +1,317 @@
+from collections import namedtuple, deque
+import numpy as np
+import pickle
+import os
+from typing import List
+
+import events as e
+from .callbacks import state_to_features, ACTIONS, STATE, features_to_index
+
+# This is only an example!
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+# Hyper parameters -- DO modify
+TRANSITION_HISTORY_SIZE = 3  # keep only ... last transitions
+RECORD_ENEMY_TRANSITIONS = 1.0  # record enemy transitions with probability ...
+
+# Events
+PLACEHOLDER_EVENT = "PLACEHOLDER"
+DECREASED_DISTANCE = "DIST_DECREASED"
+INCREASED_DISTANCE = "DIST_INCREASED"
+BOMB_DROPPED_CORNER = "BOMB_CORNER"
+PREV_ACT = 'FIRST'
+BOMB_NEAR_CRATE = 'BOMB_NEAR_CRATE'
+BOMB_NOT_NEAR_CRATE = 'BOMB_NOT_NEAR_CRATE'
+MOVED_AWAY_FROM_BOMB = 'MOVED_AWAY_FROM_BOMB'
+MOVED_TO_BOMB = 'MOVED_TO_BOMB'
+DEAD_END = 'DEAD_END'
+NOT_DEAD_END = 'NOT_DEAD_END'
+
+TOTAL_STATES = 16 ** 4 * 2
+PREV_Q = {}
+
+
+def setup_training(self):
+    """
+    Initialise self for training purpose.
+
+    This is called after `setup` in callbacks.py.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+    # Example: Setup an array that will note transition tuples
+    # (s, a, r, s')
+    self.logger.debug("Parameters set up")
+    self.alpha = 0.1
+    self.gamma = 0.9
+
+    if not os.path.isfile("Q_sa_inc_state_2.npy"):
+        self.logger.info("Setting up Q_sa_inc_state_2 function")
+        construct_q_table(TOTAL_STATES)
+        with open('Q_sa_inc_state_2.npy', 'rb') as f:
+            self.q_sa = np.load(f, allow_pickle=True)
+    else:
+        self.logger.info("Loading Q_sa_inc_state_2 function from saved state.")
+        with open('Q_sa_inc_state_2.npy', 'rb') as f:
+            self.q_sa = np.load(f, allow_pickle=True)
+    self.q_sa = self.q_sa.tolist()
+    self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE)
+
+
+def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_state: dict, events: List[str]):
+    """
+    Called once per step to allow intermediate rewards based on game events.
+
+    When this method is called, self.events will contain a list of all game
+    events relevant to your agent that occurred during the previous step. Consult
+    settings.py to see what events are tracked. You can hand out rewards to your
+    agent based on these events and your knowledge of the (new) game state.
+
+    This is *one* of the places where you could update your agent.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    :param old_game_state: The state that was passed to the last call of `act`.
+    :param self_action: The action that you took.
+    :param new_game_state: The state the agent is in now.
+    :param events: The events that occurred when going from  `old_game_state` to `new_game_state`
+    """
+    if old_game_state is None and self_action == 'BOMB':
+        events.append(BOMB_DROPPED_CORNER)
+    if old_game_state is not None:
+        events = bomb_check(new_game_state, self_action, events)
+        events = coin_dist_check(old_game_state, new_game_state, events)
+        if np.array(new_game_state['bombs']).T.size > 0:
+            events = bomb_dist_check(old_game_state, new_game_state, events)
+            events = dead_end_check(old_game_state, new_game_state, events)
+
+    self.logger.debug(f'Encountered game event(s) {", ".join(map(repr, events))} in step {new_game_state["step"]}')
+
+    reward = reward_from_events(self, events)
+    fit_models(self, old_game_state, self_action, new_game_state, reward)
+
+    # state_to_features is defined in callbacks.py
+    self.transitions.append(
+        Transition(state_to_features(old_game_state), self_action, state_to_features(new_game_state), reward))
+
+
+def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
+    """
+    Called at the end of each game or when the agent died to hand out final rewards.
+    This replaces game_events_occurred in this round.
+
+    This is similar to game_events_occurred. self.events will contain all events that
+    occurred during your agent's final step.
+
+    This is *one* of the places where you could update your agent.
+    This is also a good place to store an agent that you updated.
+
+    :param self: The same object that is passed to all of your callbacks.
+    """
+    self.logger.debug(f'Encountered event(s) {", ".join(map(repr, events))} in final step')
+    reward = reward_from_events(self, events)
+    fit_models(self, last_game_state, last_action, None, reward)
+    self.transitions.append(
+        Transition(state_to_features(last_game_state), last_action, None, reward))
+    # Store the model
+    with open("my-saved-model.pt", "wb") as file:
+        pickle.dump(self.model, file)
+
+    with open('Q_sa_inc_state_2.npy', 'wb') as f:
+        np.save(f, self.q_sa)
+
+
+def reward_from_events(self, events: List[str]) -> int:
+    """
+    *This is not a required function, but an idea to structure your code.*
+
+    Here you can modify the rewards your agent get so as to en/discourage
+    certain behavior.
+    """
+    game_rewards = {
+        e.MOVED_LEFT: -0.01,
+        e.MOVED_RIGHT: -0.01,
+        e.MOVED_UP: -0.01,
+        e.MOVED_DOWN: -0.01,
+        e.WAITED: -0.02,
+        e.INVALID_ACTION: -0.08,
+        e.BOMB_EXPLODED: 0,
+        e.BOMB_DROPPED: -0.01,
+        e.CRATE_DESTROYED: 0.2,
+        e.COIN_FOUND: 0.2,
+        e.COIN_COLLECTED: 0.6,
+        e.KILLED_OPPONENT: 2,
+        e.KILLED_SELF: -2,
+        e.GOT_KILLED: -1,
+        e.OPPONENT_ELIMINATED: 2,
+        e.SURVIVED_ROUND: 0.2,
+        # custom events
+        e.DECREASED_DISTANCE: 0.04,
+        e.INCREASED_DISTANCE: -0.02,
+        e.BOMB_DROPPED_CORNER: -0.4,
+        e.BOMB_NEAR_CRATE: 0.08,
+        e.BOMB_NOT_NEAR_CRATE: -0.06,
+        e.MOVED_AWAY_FROM_BOMB: 0.08,
+        e.MOVED_TO_BOMB: -0.04,
+        e.DEAD_END: -0.2,
+        e.NOT_DEAD_END: 0.2
+        # PLACEHOLDER_EVENT: -.1  # idea: the custom event is bad
+    }
+    reward_sum = 0
+    for event in events:
+        if event in game_rewards:
+            reward_sum += game_rewards[event]
+    self.logger.info(f"Awarded {reward_sum} for events {', '.join(events)}")
+    return reward_sum
+
+
+def fit_models(self, old_game_state, action, new_game_state, reward):
+    global PREV_Q
+    if old_game_state is None:
+        old_state_idx = 0
+    else:
+        old_state_idx = features_to_index(state_to_features(old_game_state))
+
+    if new_game_state is None:
+        PREV_Q[len(PREV_Q)] = [old_state_idx, action]
+        if reward < 0:
+            """for old_ind in range(len(PREV_Q) - 2, len(PREV_Q)):
+                model_old_q_value = self.q_sa[PREV_Q[old_ind][0]][PREV_Q[old_ind][1]]
+                old_q = self.alpha * (
+                        reward / 3 - self.gamma * model_old_q_value)
+                self.q_sa[PREV_Q[old_ind][0]][PREV_Q[old_ind][1]] += old_q"""
+        else:
+            for old_ind in range(len(PREV_Q) - 1):
+                model_old_q_value = self.q_sa[PREV_Q[old_ind][0]][PREV_Q[old_ind][1]]
+                old_q = self.alpha * (
+                        reward / len(PREV_Q) - self.gamma * model_old_q_value)
+                self.q_sa[PREV_Q[old_ind][0]][PREV_Q[old_ind][1]] += old_q
+        model_a_old_q_value = self.q_sa[old_state_idx][action]
+        old_q_value = self.alpha * (
+                reward / len(PREV_Q) - model_a_old_q_value)
+    else:
+        new_state_idx = features_to_index(state_to_features(new_game_state))
+        if reward != -0.08:
+            PREV_Q[len(PREV_Q)] = [new_state_idx, action]
+
+        model_a_new_q_values = list(self.q_sa[new_state_idx].values())
+        model_a_old_q_value = self.q_sa[old_state_idx][action]
+
+        old_q_value = self.alpha * (
+                reward + self.gamma * model_a_new_q_values[np.argmax(model_a_new_q_values)] -
+                model_a_old_q_value)
+
+    self.q_sa[old_state_idx][action] += old_q_value
+
+    """if self.q_sa[old_state_idx][action] < -10:
+        self.q_sa[old_state_idx][action] = -10"""
+    if self.q_sa[old_state_idx][action] > 1:
+        for act in ACTIONS:
+            if self.q_sa[old_state_idx][act] > 1:
+                self.q_sa[old_state_idx][act] = self.q_sa[old_state_idx][act] / self.q_sa[old_state_idx][action]
+    elif 0 < self.q_sa[old_state_idx][action] < 0.001:
+        self.q_sa[old_state_idx][action] = 0.001
+
+
+def construct_q_table(state_count):
+    Q = {}
+    for s in range(state_count):
+        Q[s] = {}
+        for a in ACTIONS:
+            if a == "WAIT" or a == "BOMB":
+                Q[s][a] = 0.2
+            else:
+                Q[s][a] = 0.2
+
+    with open('Q_sa_inc_state_2.npy', 'wb') as f:
+        np.save(f, Q)
+
+
+def coin_dist_check(old_game_state, new_game_state, events):
+    old_coins = np.array(old_game_state["coins"]).T
+    new_coins = np.array(new_game_state["coins"]).T
+    old_self_cor_channel = np.array(old_game_state["self"][3]).T
+    new_self_cor_channel = np.array(new_game_state["self"][3]).T
+    if old_coins.size != 0 and new_coins.size != 0:
+        if old_coins.size > new_coins.size:
+            max_coin = old_coins
+            min_coin = new_coins
+        else:
+            max_coin = new_coins
+            min_coin = old_coins
+        for ind in range(len(max_coin[0])):
+            for index in range(len(min_coin[0])):
+                if max_coin[0, ind] == min_coin[0, index] and max_coin[1, ind] == min_coin[1, index]:
+                    max_dist = np.abs((max_coin[0, ind] - old_self_cor_channel[1])) + np.abs(
+                        (max_coin[1, ind] - old_self_cor_channel[0]))
+                    min_dist = np.abs((min_coin[0, index] - new_self_cor_channel[1])) + np.abs(
+                        (min_coin[1, index] - new_self_cor_channel[0]))
+                    if min_dist < max_dist and max_coin.all() == old_coins.all():
+                        events.append(DECREASED_DISTANCE)
+                    elif min_dist > max_dist and max_coin.all() == old_coins.all():
+                        events.append(INCREASED_DISTANCE)
+                    elif min_dist < max_dist and min_coin.all() == old_coins.all():
+                        events.append(INCREASED_DISTANCE)
+                    elif min_dist > max_dist and min_coin.all() == old_coins.all():
+                        events.append(DECREASED_DISTANCE)
+    return events
+
+
+def bomb_check(new_game_state, self_action, events):
+    self_cord = np.array(new_game_state["self"][3])
+    if (self_cord[1] == 1 and self_cord[0] == 1) or (self_cord[1] == 1 and self_cord[0] == 15) or \
+            (self_cord[1] == 15 and self_cord[0] == 1) or (self_cord[1] == 15 and self_cord[0] == 15):
+        if self_action == 'BOMB':
+            events.append(BOMB_DROPPED_CORNER)
+    field = np.array(new_game_state["field"]).T
+    if (field[self_cord[1], self_cord[0] + 1] == 1) or (field[self_cord[1], self_cord[0] - 1] == 1) or \
+            (field[self_cord[1] + 1, self_cord[0]] == 1) or (field[self_cord[1] - 1, self_cord[0]] == 1):
+        if self_action == 'BOMB':
+            events.append(BOMB_NEAR_CRATE)
+    else:
+        if self_action == 'BOMB':
+            events.append(BOMB_NOT_NEAR_CRATE)
+    return events
+
+
+def bomb_dist_check(old_game_state, new_game_state, events):
+    old_self_cor_channel = np.array(old_game_state["self"][3])
+    new_self_cor_channel = np.array(new_game_state["self"][3])
+    for bomb in old_game_state['bombs']:
+        bomb_location = bomb[0]
+
+        old_dist_bomb = np.sum(np.abs(bomb_location - old_self_cor_channel))
+        new_dist_bomb = np.sum(np.abs(bomb_location - new_self_cor_channel))
+        if old_dist_bomb < 5:
+            if new_dist_bomb > old_dist_bomb:
+                events.append('MOVED_AWAY_FROM_BOMB')
+            else:
+                events.append('MOVED_TO_BOMB')
+    return events
+
+
+def dead_end_check(old_game_state, new_game_state, events):
+    new_own_location = np.asarray(new_game_state['self'][-1])
+    old_own_location = np.asarray(old_game_state['self'][-1])
+    for bomb in old_game_state['bombs']:
+        bomb_location = bomb[0]
+        if tuple(old_own_location) == bomb_location:
+            chosen_direction = new_own_location - old_own_location
+            if np.sum(chosen_direction) != 0:
+                free_tiles = [(x, y) for (x, y) in np.asarray(np.where(old_game_state['field'] == 0)).T]
+                turn_direction = chosen_direction[::-1]
+
+                loc_dummy = np.copy(new_own_location)
+                dead_end = True
+                for i in range(3):
+                    if tuple(loc_dummy + turn_direction) in free_tiles or tuple(
+                            loc_dummy - turn_direction) in free_tiles:
+                        dead_end = False
+                        break
+                    else:
+                        loc_dummy = loc_dummy + chosen_direction
+                if dead_end:
+                    events.append('DEAD_END')
+                else:
+                    events.append('NOT_DEAD_END')
+    return events
